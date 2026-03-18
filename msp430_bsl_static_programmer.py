f0008a318ec09d00d2cf4a94cf10b9a07455c7ca
Alfonso Amaya <alfonsocamargo0108@gmail.com>
2026-03-18 12:03:20 -0600

    Fix static BSL programm loader

--- msp430_bsl_static_programmer.py
+++ msp430_bsl_static_programmer.py
@@ -825,11 +825,7 @@ def main():
         filepath = None
 
     bsl = MSP430_BSL(port=port, baudrate=args.baud)
-
-    # Solicitar modo bypass UART a Mission Boss
-    bsl.request_mb_bypass_uart_mode()
-    time.sleep(1)
-
+    
     # Programar: hardcodeado si no se pasa -f, TI-TXT si se pasa
     if args.file:
         success = bsl.program_ti_txt(args.file)
